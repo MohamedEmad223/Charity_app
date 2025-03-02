d06e6f98cc73708544bd381b3d8f786b9cbd3d9c
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-03-02 23:23:49 +0200

    add home images

--- lib/feature/home/presentation/screens/home_screen.dart
+++ lib/feature/home/presentation/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:charity_app/core/helpers/app_images.dart';
+import 'package:charity_app/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +9,36 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+      body: Column(
+        children: [
+          Container(
+            width: 375.w,
+            height: 187.h,
+            decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40.r),
+                    bottomRight: Radius.circular(40.r))),
+            child: Column(
+              children: [
+                SizedBox(height: 40.h),
+                Row(
+                  children: [
+                    Center(
+                      child: Image.asset(
+                        AppImages.homeImages,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                    Align(
+                        alignment: Alignment.topRight,
+                        child: Image.asset(AppImages.searchImages))
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
